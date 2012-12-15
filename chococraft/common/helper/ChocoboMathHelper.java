ec19ba64150db4ccf0d2a985432a2f461c47c9fa
Arno Saxena <al-s@gmx.de>
2012-12-15 16:48:07 +0100

    new methods for one sided limit in MathHelper class

--- chococraft/common/helper/ChocoboMathHelper.java
+++ chococraft/common/helper/ChocoboMathHelper.java
@@ -2,6 +2,7 @@ package chococraft.common.helper;
 
 public class ChocoboMathHelper
 {
+	// *** clamp ***
     // numeric float clamp
     public static float clamp(float value, float min, float max)
     {
@@ -18,5 +19,43 @@ public class ChocoboMathHelper
     public static int clamp(int value, int min, int max)
     {
         return (value < min ? min : (value > max ? max : value));
+    }
+    
+    // *** minimum limit ***
+    // numeric float minimum limit
+    public static float minLimit(float value, float min)
+    {
+    	return value < min ? min : value;
+    }
+    
+    // numeric double minimum limit
+    public static double minLimit(double value, double min)
+    {
+    	return value < min ? min : value;
+    }
+    
+    // numeric double minimum limit
+    public static int minLimit(int value, int min)
+    {
+    	return value < min ? min : value;
+    }
+    
+    // *** maximum limit ***
+    // numeric float maximum limit
+    public static float maxLimit(float value, float max)
+    {
+    	return value > max ? max : value;
+    }
+    
+    // numeric double maximum limit
+    public static double maxLimit(double value, double max)
+    {
+    	return value > max ? max : value;
+    }
+    
+    // numeric double maximum limit
+    public static int maxLimit(int value, int max)
+    {
+    	return value > max ? max : value;
     }
 }
