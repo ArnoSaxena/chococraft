8bc35001ebea4fb0735ce4487056ad60c819e2c4
Arno Saxena <al-s@gmx.de>
2012-12-18 15:28:53 +0100

    events
    sound from zip and fire damage prevention when riding purple

--- chococraft/common/ChocoCraftEvent.java
+++ chococraft/common/ChocoCraftEvent.java
@@ -1,7 +1,13 @@
 package chococraft.common;
 
+import net.minecraft.src.DamageSource;
+import net.minecraft.src.EntityPlayer;
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.BonemealEvent;
+import chococraft.common.entities.colours.EntityChocoboPurple;
 import chococraft.common.helper.ChocoboPlayerHelper;
 import chococraft.common.items.BlockGysahlStem;
 
@@ -19,24 +25,34 @@ public class ChocoCraftEvent
 		}
 	}
 
-//    @ForgeSubscribe
-//    public void onUserConnect(PlayerEvent event)
-//    {
-//    	if(event.entity instanceof EntityPlayer)
-//    	{
-//    	}
-//    }
-
-//    @ForgeSubscribe
-//    public void onSound(SoundLoadEvent event)
-//    {
-//        try
-//        {
-//            event.manager.soundPoolSounds.addSound("chocobokweh", ModChocoCraft.class.getResource(Constants.CHOCOBO_SOUND_FOLDER + "/choco_kweh.ogg"));            
-//        }
-//        catch (Exception e)
-//        {
-//            System.err.println("Failed to register one or more sounds.");
-//        }
-//    }
+    @ForgeSubscribe
+    public void onSound(SoundLoadEvent event)
+    {
+        try
+        {
+            event.manager.soundPoolSounds.addSound("choco_kweh.ogg", ModChocoCraft.class.getResource(Constants.CHOCOBO_SOUND_FOLDER + "/choco_kweh.ogg"));            
+        }
+        catch (Exception e)
+        {
+            System.err.println("Failed to register one or more sounds.");
+        }
+    }
+    
+    @ForgeSubscribe
+    public void onBurnDamage(LivingAttackEvent event)
+    {
+    	if(event.entity instanceof EntityPlayer)
+    	{
+    		EntityPlayer player = (EntityPlayer)event.entity;
+    		if(event.source.equals(DamageSource.lava) 
+    				|| event.source.equals(DamageSource.inFire) 
+    				|| event.source.equals(DamageSource.onFire))
+    		{
+    			if(player.ridingEntity instanceof EntityChocoboPurple)
+    			{
+    				event.setCanceled(true);
+    			}
+    		}
+    	}
+    }
 }
