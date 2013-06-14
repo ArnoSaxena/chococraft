1f583cd3f403c02a8853e100eda0e5b9c5c77967
Arno Saxena <al-s@gmx.de>
2013-06-14 13:21:50 +0200

    added head comments

--- chococraft/common/helper/ChocoboPlayerHelper.java
+++ chococraft/common/helper/ChocoboPlayerHelper.java
@@ -1,3 +1,17 @@
+// <copyright file="ChocoboPlayerHelper.java">
+// Copyright (c) 2013 All Right Reserved, http://chococraft.arno-saxena.de/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Arno Saxena</author>
+// <email>al-s@gmx.de</email>
+// <date>2013-06-08</date>
+// <summary>Static helper class for methods connected to player activities</summary>
+
 package chococraft.common.helper;
 
 import net.minecraft.entity.player.EntityPlayer;
