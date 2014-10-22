44d93a122c75b6276e314c9a5b1eebbcdeff46a0
Clienthax <clienthax@gmail.com>
2014-10-22 21:35:10 +0100

    Merge packet helpers.
    Signed-off-by: Clienthax <clienthax@gmail.com>

--- src/main/java/chococraft/common/network/PacketHelper.java
+++ src/main/java/chococraft/common/network/PacketHelper.java
@@ -4,6 +4,7 @@ import chococraft.common.entities.EntityAnimalChocobo;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
@@ -20,6 +21,9 @@ public class PacketHelper {
 		if (Side.CLIENT == FMLCommonHandler.instance().getEffectiveSide())
 		{
 			targetEntity = (Entity) FMLClientHandler.instance().getClient().theWorld.getEntityByID(entityId);
+		}
+		else {
+			targetEntity = MinecraftServer.getServer().worldServerForDimension(dimension).getEntityByID(entityId);
 		}
 
 		if(null != targetEntity && targetEntity instanceof EntityAnimalChocobo)
@@ -39,6 +43,11 @@ public class PacketHelper {
 			WorldServer worldserver = MinecraftServer.getServer().worldServerForDimension(dimension);
 			return worldserver.getPlayerEntityByName(name);
 		}
+		if (Side.CLIENT == FMLCommonHandler.instance().getEffectiveSide())
+		{
+			return FMLClientHandler.instance().getClient().theWorld.getPlayerEntityByName(name);
+		}
+
 		return null;
 	}
 }
